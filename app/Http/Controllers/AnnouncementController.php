82ffc604a722340463977fcadbd2bda4435dc321
Vlad Chirila <vladchirila6@gmail.com>
2021-05-12 10:35:30 +0200

    aggiunte regole e messaggi di errore personalizzati per la validazione del form di creazione dell'annuncio

--- app/Http/Controllers/AnnouncementController.php
+++ app/Http/Controllers/AnnouncementController.php
@@ -3,13 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Http\Requests\AnnouncementRequest;
 
 class AnnouncementController extends Controller
 {
     public function create () {
         return view('announcements.create');
     }
-    public function store (Request $request) {
-        dd($request->all());
+    public function store (AnnouncementRequest $request) {
+        $announcement= AnnouncementRequest::create();
+        return redirect()->back()->with('message', "Il tuo annuncio è stato creato con successo.");
     }
 }
+
--- app/Http/Requests/AnnouncementRequest.php
+++ app/Http/Requests/AnnouncementRequest.php
@@ -13,7 +13,7 @@ class AnnouncementRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,25 @@ class AnnouncementRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            "title"=> "required|string|max:120", 
+            "body" =>"required|string|max:500",
+            "category_id" => "required",
+        ];
+    }
+    //messaggi di errore personalizzati
+    public function messages()
+    {
+        return [
+            "title.required"=> "Il titolo è richiesto.",
+            "title.string"=> "Il titolo deve contenere del testo.",
+            "title.max"=> "Il titolo deve contenere massimo :max caratteri.",
+            "body.required" =>"L'articolo è richiesto.",
+            "body.string" =>"L'articolo deve contenere del testo.",
+            "body.max" =>"L'articolo deve contenere massimo :max caratteri.",
+            "category_id.required" => "Non hai scelto una categoria.",
         ];
     }
 }
+
+
+
