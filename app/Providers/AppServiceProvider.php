ba116e33243dd40a22fb8601774ee63b98d64ed1
Roberto Raspanti <ras.roberto95@gmail.com>
2021-05-12 09:55:07 +0200

    creata AnnouncementRequest

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -25,7 +25,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $categories = Category::all();
+       $categories = Category::all();
         View::share('categories', $categories);
     }
 }
