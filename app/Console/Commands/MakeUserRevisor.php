25f70c73b5b5ce2698a0a95d71ba698a3985995a
Vlad Chirila <vladchirila6@gmail.com>
2021-05-13 16:36:40 +0200

    flag is revisor aggiunto e creato comando makeuserrevisor

---
+++ app/Console/Commands/MakeUserRevisor.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+
+class MakeUserRevisor extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'presto:makeUserRevisor';
+    protected $description = 'Rendi un utente revisore';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $email = $this->ask("Inserisci l'email dell'utente che vuoi rendere revisore")
+        
+    }
+}
