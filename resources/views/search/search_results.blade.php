f1fcc8be9ba1c91216fdc56eb2f600bdbd814ec1
Angelo <angelo.adduci1991@gmail.com>
2021-05-15 16:15:33 +0200

    comando per creare announcements.index in storage per funzionamento search scout. completamento della vista search_results con aggiunta di un header con contenuto ricerca e value nell'imput del contenuto ricerca

--- resources/views/search/search_results.blade.php
+++ resources/views/search/search_results.blade.php
@@ -1,11 +1,14 @@
 <x-layout>
     <div class="container-fluid px-5">
         <div class="row">
+            <div class="col-md-12 fs-5">
+                Ecco i risultati della ricerca per <span class="main-text fs-4">{{$q}}</span>
+            </div>
             <div class="col-md-12">
                 <div class="input-group mb-3">
                     <form method="GET" action="{{route('search')}}">
                         @csrf
-                        <input type="text" name="q" class="form-control" placeholder="Ricerca" aria-label="Ricerca" aria-describedby="button-addon2" >
+                        <input type="text" value="{{$q}}" name="q" class="form-control" placeholder="Ricerca" aria-label="Ricerca" aria-describedby="button-addon2" >
                         <button class="btn main-bg text-white" type="submit" id="button-addon2">Button</button>
                     </form>
                 </div>
