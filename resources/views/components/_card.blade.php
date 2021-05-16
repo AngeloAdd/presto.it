c2d225e11b91613a72c18a304bee203425ea1ee0
Angelo <angelo.adduci1991@gmail.com>
2021-05-16 16:36:23 +0200

    aggiunta subnav con categorie e header con barra di ricerca sistemato nel layout decisi colori principali e font principale del progetto e inseriti nel file css. sistemato layout card in maniera responsive.

--- resources/views/components/_card.blade.php
+++ resources/views/components/_card.blade.php
@@ -1,19 +1,28 @@
-<div class="col-md-4 mb-4">
-    <div class="card mb-3">
-        <div class="row g-0">
-            <div class="col-md-6">
-                <img class="w-100 h-100" src="https://via.placeholder.com/150" alt="...">
+<div class="col-12 col-sm-6 col-xl-4 mb-4">
+    <div class="card mb-3 shadow rounded-1">
+
+        <div class="row ps-lg-4">
+            <div class="col-12 col-lg-6 d-flex flex-column justify-content-around align-items-center py-lg-4">
+                <img class="d-lg-flex w-100" src="https://picsum.photos/300" alt="...">
             </div>
-            <div class="col-md-6">
-                <div class="card-body d-flex flex-column align-items-start justify-content-center">
+            <div class="col-12 col-lg-6 ps-lg-0">
+                <div class="card-body d-flex flex-column align-items-start justify-content-between h-100">
                     <h3 class="card-title text-bold main-text mb-0">{{$announcement->title}}</h3>
-                    <p class="card-text"><small class="text-muted">Creato da: {{$announcement->user->name}}</small></p>
-                    <p class="card-text">{{$announcement->body}}</p>
-                    <p class="my-1 ml-5 text-secondary">Prezzo: <span class="fs-5 main-text">{{$announcement->price}}€ </span></p>
-                    <a class="text-decoration-none text-secondary my-2" href="{{route('announcements.show', ["category"=>$announcement->category])}}">Categoria: <span class="fs-5 sec-text">{{$announcement->category->name}}</span></a>
-                
-                    <a href="#" class="btn sec-bg text-white">Visualizza</a>
-                    <p class="card-text mb-0 w-100 d-flex justify-content-end"><small class="text-muted">Creato il: {{$announcement->created_at->format('d/m/Y')}}</small></p>
+                    <small class="card-text sec-text mb-0">
+                        Creato da: {{$announcement->user->name}}
+                    </small>
+                    <a class="text-decoration-none acc-text" href="{{route('announcements.show', ["category"=>$announcement->category])}}">
+                        <small class="sec-text">Categoria: </small>
+                        {{$announcement->category->name}}
+                    </a>
+                    <p class="card-text acc-text">{{substr($announcement->body,0,10)}}</p>
+                    <div class="d-flex justify-content-start align-item-center pb-2">
+
+                        <p class="sec-text mb-0 d-flex align-items-center">Prezzo:</p>
+                        <h3 class="main-text ms-2 mb-0 d-flex align-items-center">{{$announcement->price}}€ </h3>
+                    </div>
+                        <a href="#" class="justify-self-end btn main-bg text-white">Visualizza</a>
+                    <p class="card-text mb-0 mt-2 w-100 d-flex justify-content-end"><small class="sec-text">Creato il: {{$announcement->created_at->format('d/m/Y')}}</small></p>
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,18 +1,22 @@
 <x-layout>
-    <div class="container-fluid px-5">
-        <div class="row">
-            <div class="col-md-12">
-                <div class="input-group mb-3">
-                    <form method="GET" action="{{route('search')}}">
-                        @csrf
-                        <input type="text" name="q" class="form-control" placeholder="Ricerca" aria-label="Ricerca" aria-describedby="button-addon2" >
-                        <button class="btn main-bg text-white" type="submit" id="button-addon2">Button</button>
-                    </form>
-                </div>
-            </div>
-        @foreach ($announcements as $announcement)
-            @include('components._card')
-        @endforeach
+
+    <div class="vh-100 w-100 d-flex justify-content-center align-items-center above-the-fold">
+
+        <div class="mb-3">
+            <form method="GET" action="{{route('search')}}">
+                @csrf
+                <input type="text" name="q" class="form-control w-100" placeholder="Ricerca" aria-label="Ricerca" aria-describedby="button-addon2" >
+                <button class="btn main-bg text-white" type="submit" id="button-addon2">Button</button>
+            </form>
+        </div>
+
+    </div>
+
+    <div class="container-fluid px-5 mt-5">
+        <div class="row w-100">
+            @foreach ($announcements as $announcement)
+                @include('components._card')
+            @endforeach
         </div>
     </div>
 </x-layout>
