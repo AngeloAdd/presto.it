b9cd54c04b8dace24ec430ff0680102aeba33c53
Angelo <angelo.adduci1991@gmail.com>
2021-05-20 23:34:22 +0200

    index revisore e modifiche a welcome

--- resources/views/revisors/index.blade.php
+++ resources/views/revisors/index.blade.php
@@ -1,113 +1,167 @@
 <x-layout>
+    <x-slot name="style">
+        <style>
+        hr{
+            color: var(--sec-color);
+        }
+
+        .container {
+            min-height: 100vh;
+        }
+
+        .btn-custom {
+            background-color: var(--acc-color);
+            color: white;
+            position: relative;
+            overflow: hidden;
+
+        }
+
+        .btn-custom span {
+            color: white;
+            position: relative;
+        }
+
+        .btn-custom::before {
+            content: "";
+            position: absolute;
+            top: 0;
+            left: 0;
+            width: 100%;
+            height: 100%;
+            background-color: var(--main-color);
+            transform: translateX(-100%);
+            transition: transform 0.4s;
+        }
+
+        .btn-custom:hover::before {
+            transform: translateX(0)
+        }
+
+        
+
+        </style>
+    </x-slot>
     @if ($announcement)
-    <div class="container-fluid px-5 py-5">
-        <div class="row d-flex justify-content-center align-items-center">
-            <div class="col-12 col-sm-6 col-xl-4 mb-4">
-                <div class="card mb-3 shadow rounded-1">
-                    <div class="row ps-lg-4">
-                        <div class="col-12 col-lg-6 ps-lg-0">
-                            <div class="card-body d-flex flex-column align-items-start justify-content-between h-100">
-                                <h3 class="card-title text-bold main-text mb-0">{{$announcement->title}}</h3>
-                                <small class="card-text sec-text mb-0">
-                                    Creato da: {{$announcement->user->name}}
-                                </small>
-                                <a class="text-decoration-none acc-text" href="{{route('announcements.show', ["category"=>$announcement->category])}}">
-                                    <small class="sec-text">Categoria: </small>
-                                    {{$announcement->category->name}}
-                                </a>
-                                <p class="card-text acc-text">{{substr($announcement->body,0,10)}}</p>
-                                <div class="d-flex justify-content-start align-item-center pb-2">
-
-                                    <p class="sec-text mb-0 d-flex align-items-center">Prezzo:</p>
-                                    <h3 class="main-text ms-2 mb-0 d-flex align-items-center">{{$announcement->price}}€ </h3>
-                                </div>
-                                    <a href="{{route('announcement.show', compact('announcement'))}}" class="justify-self-end btn main-bg text-white">Visualizza</a>
-                                <p class="card-text mb-0 mt-2 w-100 d-flex justify-content-end"><small class="sec-text">Creato il: {{$announcement->created_at->format('d/m/Y')}}</small></p>
-                            </div>
-                        </div>
-                        @if ($announcement->announcementImages)
-                            <div class="col-12 d-flex flex-column justify-content-around align-items-center py-lg-4">
-                                <div class="container">
-                                    <div class="row">
-                                        @foreach ($announcement->announcementImages as $image)
-                                            <div class="col-6">
-                                                <img src="{{$image->getUrl(250,250)}}" alt="">
-                                            </div>
-                                            <div class="col-6">
-                                                <ul class="list-group">
-                                                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div class="ms-2 me-auto">
-                                                            <div class="fw-bold">Adult</div>
-
-                                                        </div>
-                                                        <span class="badge bg-primary rounded-pill">{{$image->adult}}</span>
-                                                    </li>
-                                                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div class="ms-2 me-auto">
-                                                            <div class="fw-bold">Medical</div>
-
-                                                        </div>
-                                                        <span class="badge bg-primary rounded-pill">{{$image->medical}}</span>
-                                                    </li>
-                                                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div class="ms-2 me-auto">
-                                                            <div class="fw-bold">Spoof</div>
-
-                                                        </div>
-                                                        <span class="badge bg-primary rounded-pill">{{$image->spoof}}</span>
-                                                    </li>
-                                                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div class="ms-2 me-auto">
-                                                            <div class="fw-bold">Violence</div>
-
-                                                        </div>
-                                                        <span class="badge bg-primary rounded-pill">{{$image->violence}}</span>
-                                                    </li>
-                                                    <li class="list-group-item d-flex justify-content-between align-items-start">
-                                                        <div class="ms-2 me-auto">
-                                                            <div class="fw-bold">Racy</div>
-
-                                                        </div>
-                                                        <span class="badge bg-primary rounded-pill">{{$image->racy}}</span>
-                                                    </li>
-                                                </ul>
-                                            </div>
-                                            <div class="col-12">
-                                                <ul class="list-unstyled">
-                                                    @if ($image->labels)
-                                                        @foreach ($image->labels as $label)
-                                                    <li>{{$label}}</li>
-                                                        @endforeach
-                                                    @endif
-                                              </ul>
-                                            </div>
-                                        @endforeach
-                                    </div>
-                                </div>
-                                <!-- <img class="d-lg-flex w-100" src="https://picsum.photos/300" alt="..."> -->
-                            </div>
-                        @endif
-                    </div>
-                </div>
+        <div class="d-flex justify-content-center mt-5">
+            <h3> Ci sono <span class="badge rounded-pill main-bg">{{\App\Models\Announcement::toBeRevised()}}</span> annunci da revisionare</h3>
+        </div>
+        <div class="container py-5 px-5 shadow mx-5 mb-5">
+        <div class="row my-2 px-5">
+            <div class="col-12 col-md-3">
+                <h4>Titolo</h4>
+            </div>
+            <div class="col-12 col-md-9 main-text">
+                <h3>{{$announcement->title}}</h3>
+                <small class="sec-text">Creato il: {{$announcement->created_at->format('d/m/Y')}}</small>
             </div>
         </div>
-        <div class="row">
-            <div class="col-6 d-flex justify-content-center align-items-center">
+        <hr>
+        <div class="row my-2 px-5">
+            <div class="col-12 col-md-3">
+                <h4>Descrizione</h4>
+            </div>
+            <div class="col-12 col-md-9">
+                <p>{{$announcement->body}}</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row my-2 px-5">
+            <div class="col-12 col-md-3">
+                <h4>Prezzo</h4>
+            </div>
+            <div class="col-12 col-md-9">
+                <h4 class="main-text">€ {{$announcement->price}}</h4>
+            </div>
+        </div>
+        <hr>
+        <div class="row my-2 px-5">
+            <div class="col-12 col-md-3">
+                <h4>Categoria</h4>
+            </div>
+            <div class="col-12 col-md-9 sec-text">
+                <p>{{$announcement->category->name}}</p>
+            </div>
+        </div>
+        <hr>
+        <div class="row my-2 px-5">
+            <div class="col-12 col-md-3">
+                <h4>Immagini</h4>
+            </div>
+            <div class="col-12 col-md-9">
+                @foreach ($announcement->announcementImages as $image)
+                    <div class="row flex-row">
+                        <div class="col-12 col-lg-4 my-3">
+                            <img src="{{$image->getUrl(250,250)}}" alt="" srcset="">                    
+                        </div>
+                        <div class="col-12 col-lg-8 my-3">
+                            <ul class="list-group list-group-flush">
+                                <li class="d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold acc-text acc-text">Adult</div>
+                                    </div>
+                                    <p class="badge sec-bg rounded-pill">{{$image->adult}}</p>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold acc-text">Medical</div>
+
+                                    </div>
+                                    <p class="badge sec-bg rounded-pill">{{$image->medical}}</p>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold acc-text">Spoof</div>
+
+                                    </div>
+                                    <p class="badge sec-bg rounded-pill">{{$image->spoof}}</p>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold acc-text">Violence</div>
+
+                                    </div>
+                                    <p class="badge sec-bg rounded-pill">{{$image->violence}}</p>
+                                </li>
+                                <li class="d-flex justify-content-between align-items-start">
+                                    <div class="ms-2 me-auto">
+                                        <div class="fw-bold acc-text">Racy</div>
+                                    </div>
+                                    <p class="badge sec-bg rounded-pill">{{$image->racy}}</p>
+                                </li>
+                            </ul>
+                            <section class="my-3">
+                                <span class="main-text fw-bold">Labels:</span>
+                                    @if ($image->labels)
+                                    @foreach ($image->labels as $label)
+                                        {{$label}},
+                                    @endforeach
+                                    @endif                                
+                            </section>            
+                        </div>
+                    </div>
+                    <hr>
+                @endforeach    
+            </div>
+        </div>
+        <div class="row pt-5">
+            <div class="col-6 d-flex justify-content-end align-items-center">  
                 <form action="{{route('revisor.reject', compact('announcement'))}}" method="POST">
                     @csrf
-                    <button class="btn btn-danger" type="submit">Cestino</button>
-                </form>
+                    <button class="btn btn-custom fs-3" type="submit"><span>Cestino</span></button>
+                </form>              
             </div>
-            <div class="col-6 d-flex justify-content-center align-items-center">
+            <div class="col-6 d-flex justify-content-start align-items-center">
             <form  action="{{route('revisor.accept', compact('announcement'))}}" method="POST">
                 @csrf
-                    <button class="btn btn-success d-flex" type="submit">Accetta</button>
+                    <button class="btn btn-custom fs-3" type="submit"><span>Accetta</span></button>
             </form>
             </div>
         </div>
-    </div>
-    @else
-        <h3>Nessun articolo da revisionare</h3>
-    @endif
-
+        </div>
+        @else
+        <div class="d-flex justify-content-center align-items-center my-4 ">            
+            <h3>Nessun articolo da revisionare</h3>               
+        </div>
+        @endif
 </x-layout>
